8135fb4cc7c20b2bbbdc410786802560c85af4e3
Bel-Ozajasz <72888217+Bel-Ozajasz@users.noreply.github.com>
2020-10-14 21:12:49 +0200

    Add files via upload

---
+++ convalues/ideologies.js
@@ -0,0 +1,39 @@
+ideologies = [
+    {
+        "name": "Łzy Aszary",
+        "stats": {
+            "econ": 60,
+            "dipl": 80,
+            "govt": 60,
+            "scty": -90
+        }
+    },
+	{
+        "name": "Blask Naiuny",
+        "stats": {
+            "econ": 50,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 50
+        }
+    },
+	{
+        "name": "Salam-Eun",
+        "stats": {
+            "econ": 60,
+            "dipl": 60,
+            "govt": 80,
+            "scty": -60
+        }
+    },
+	{
+        "name": "Eiran",
+        "stats": {
+            "econ": 90,
+            "dipl": -80,
+            "govt": -70,
+            "scty": 70
+        }
+    },
+	
+];
---
+++ convalues/questions.js
@@ -0,0 +1,148 @@
+questions = [
+    {
+        "question": "Mój con to dobre miejsce do życia.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+	{
+        "question": "W moim conie istnieje jakaś siła wyższa.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+	{
+        "question": "Wszystko w moim conie ma swoje logiczne wyjaśnienie.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+	{
+        "question": "Mój con jest przede wszystkim produktem mojej wyobraźni. Jeśli logika jej przeczy tym gorzej dla logiki.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+	{
+        "question": "Jeśli w moim conie ma być magia to powinna być ona twardym systemem.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -5,
+            "scty": 10
+        }
+    },
+	{
+        "question": "Mój con zawiera alegorie lub metafory.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+	{
+        "question": "Preferuję czerpać z tradycji i duchowości niż z nauki i współczesnej fantastyki.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+	{
+        "question": "Con powinien być realistyczny",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 5
+        }
+    },
+	{
+        "question": "Problematyka jest ważniejsza niż linia brzegowa Zadupiogównogrodu",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 5,
+            "scty": 0
+        }
+    },
+	{
+        "question": "W moim conie dobro zawsze ostatecznie zwycięża",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+		}
+    },
+
+	{
+        "question": "Lubuję się w sprawianiu cierpienia moim conludkom",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+
+	{
+        "question": "W moim conie często dotykam tematów takich jak śmierć, depresja, gwałt itp.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+	{
+        "question": "Fantastyka kiedyś była lepsza niż teraz.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+	{
+        "question": "Fizyka kwantowa to lepszy system dla cona niż astrologia lub alchemia",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+	{
+        "question": "Inspiruję estetykę magii w moim conie na ezoteryce lub tradycyjnych wierzeniach.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+	{
+        "question": "Zawieszenie niewiary powinno być zredukowane do minimum",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+];
